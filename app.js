2a93b887c609a1a5b87880d3563336bbda298cd1
daviddubliakov <dubliakov1@gmail.com>
2022-10-17 15:18:12 +0300

    fixes app listen method

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
@@ -13,6 +11,5 @@ app.use((req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello from Express</h1>');
 });
-const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
