166a4ca24bf5485f8779521116ab5b42e63d7f4a
daviddubliakov <dubliakov1@gmail.com>
2022-10-17 14:23:52 +0300

    section 3 assignment

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http');
-
 const routes = require('./routes');
 
-const server = http.createServer(routes.handler);
+const server = http.createServer(routes);
 
 server.listen(3000);
--- routes.js
+++ routes.js
@@ -1,45 +1,51 @@
-
-const fs = require('fs');
-
-const requestHandler = (req, res) => {
+const requestListener = (req, res) => {
   const url = req.url;
   const method = req.method;
+
+  res.setHeader('Content-Type', 'text/html');
+
   if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
-    res.write('</html>');
+    res.write('<html><body>');
+    res.write('<h1>Hello user!</h1>');
+    res.write('<form action="/create-user" method="POST"><input type="text" name="username" /><button type="submit">Create a user</button></form>');
+    res.write('</body></html>');
+
     return res.end();
   }
 
-  if (url === '/message' && method === 'POST') {
+  if (url === '/users') {
+    const DUMMY_USERS = ['David', 'Jack', 'Will'];
+    res.write('<html><body>');
+    res.write('<ul>');
+
+    DUMMY_USERS.forEach((userName) => {
+      res.write(`<li>${userName}</li>`);
+    });
+
+    res.write('</ul>');
+    res.write('</body></html>');
+    return res.end();
+  }
+
+  if (url === '/create-user' && method === 'POST') {
     const body = [];
 
     req.on('data', (chunk) => {
       body.push(chunk);
     });
+
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
+      const userName = parsedBody.split('=')[1];
 
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
+      console.log(userName);
 
-        return res.end();
-      });
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+
+      return res.end();
     });
   }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello, from my Node.js server!</h1></body>');
-  res.write('</html>');
-  res.end();
 }
 
-module.exports = {
-  handler: requestHandler,
-  someText: 'Some hard coded text',
-};
+module.exports = requestListener;
