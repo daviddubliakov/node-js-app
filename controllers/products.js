a2e81b7918d6ed098ba38c78249b08c435308692
daviddubliakov <dubliakov1@gmail.com>
2022-11-02 12:01:38 +0300

    file stroraging via models

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,17 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (_req, res) => {
   res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
 }
 
 exports.postAddProduct = (req, res) => {
-  products.push({ title: req.body.title })
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (_req, res) => {
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true });
+  Product.fetchAll((products) => {
+    res.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true });
+  });
 }
-
-exports.products = products;
