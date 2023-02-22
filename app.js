ea875721faa654e5ad17d39140a15e4333bdde26
daviddubliakov <dubliakov1@gmail.com>
2023-02-22 13:18:19 +0200

    adds auth view and controller

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,7 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://david:david2002@cluster0.i8xwsri.mongodb.net/shop?retryWrites=true&w=majority')
