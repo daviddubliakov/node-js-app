febf8e6530f839778a4b9eee27fc8c974bf3e303
daviddubliakov <dubliakov1@gmail.com>
2023-02-27 18:33:10 +0200

    adds session store

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,12 @@
 exports.getLogin = (req, res) => {
-  const isAuthenticated = req.get('Cookie').split('; ').find(c => c.includes('loggedIn')).split('=')[1] === 'true';
-  
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated
+    isAuthenticated: false
   })
 };
 
 exports.postLogin = (req, res) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true')
+  req.session.isLoggedIn = true;
   res.redirect('/');
 }; 
