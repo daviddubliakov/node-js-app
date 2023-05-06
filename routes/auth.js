1686828f7c10925bbd31b8cb796019a7af0830ee
david.dub <david.dub@brainstack.team>
2023-05-06 13:17:07 +0300

    adds check for field equality

--- routes/auth.js
+++ routes/auth.js
@@ -15,6 +15,13 @@ router.route('/signup')
     [
       body('email').isEmail().withMessage('Please enter a valid email.'),
       body('password').isLength({ min: 5 }).withMessage('Please eneter a password with at least 5 charachters.').isAlphanumeric().withMessage('Please enter a password with only numbers and text.'),
+      body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords should be equal!')
+        }
+
+        return true;
+      })
     ],
     authController.postSignup
   );
