011aa9c0e9de655de3d5ae8c34a22555db7e461e
david.dub <david.dub@brainstack.team>
2023-05-10 15:03:17 +0300

    adds express error handling middleware

--- app.js
+++ app.js
@@ -70,6 +70,10 @@ app.use(authRoutes);
 app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
+app.use((error, req, res, next) => {
+  res.redirect('/500');
+});
+
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
--- controllers/admin.js
+++ controllers/admin.js
@@ -19,7 +19,7 @@ exports.getAddProduct = (req, res) => {
   });
 };
 
-exports.postAddProduct = (req, res) => {
+exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, price, description } = req.body;
   const errors = validationResult(req);
 
@@ -57,7 +57,9 @@ exports.postAddProduct = (req, res) => {
       res.redirect('/admin/products');
     })
     .catch((err) => {
-      res.redirect('/500');
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -83,7 +85,11 @@ exports.getEditProduct = (req, res) => {
         validationErrors: [],
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postEditProduct = (req, res) => {
