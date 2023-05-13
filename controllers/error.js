e10e5566232e87e168161e8b73a56d5f950bc16b
david.dub <david.dub@brainstack.team>
2023-05-13 14:36:03 +0300

    adds a file picker to the front

--- controllers/error.js
+++ controllers/error.js
@@ -1,7 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 
 exports.get404 = (req, res, next) => {
-  res.status(404).render('404', {
+  res.status(StatusCodes.NOT_FOUND).render('404', {
     pageTitle: 'Page Not Found',
     path: '/404',
   });
