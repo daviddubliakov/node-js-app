a13877c25c6d03f1041753258dd4babf9db1431f
david.dub <david.dub@brainstack.team>
2023-07-08 16:09:41 +0300

    adds downloading files with authentication

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -143,4 +146,18 @@ exports.getOrders = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const { orderId } = req.params;
+  const invoiceName = `invoice-${orderId}.pdf`;
+  const invoicePath = path.join('data', 'invoices', invoiceName);
+
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      next(err);
+    }
+
+    res.send(data);
+  });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -20,6 +20,7 @@ router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
 router.post('/create-order', isAuth, shopController.postOrder);
 
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
 router.get('/orders', isAuth, shopController.getOrders);
 
 router.get('/', shopController.getIndex);
