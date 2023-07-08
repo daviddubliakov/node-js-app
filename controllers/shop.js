c94b2a15d8e8d800c1900e68c84ea350f074351b
david.dub <david.dub@brainstack.team>
2023-07-09 00:12:55 +0300

    adds pdfkit

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -163,15 +164,17 @@ exports.getInvoice = (req, res, next) => {
 
       const invoiceName = `invoice-${orderId}.pdf`;
       const invoicePath = path.join('data', 'invoices', invoiceName);
+      const pdfDoc = new PDFDocument();
 
-      const file = fs.createReadStream(invoicePath);
       res.setHeader('Content-Type', 'application/pdf');
       res.setHeader(
         'Content-Disposition',
         `attachment; filename=${invoiceName}`,
       );
-
-      file.pipe(res);
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+      pdfDoc.text('Hello World!');
+      pdfDoc.end();
 
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
