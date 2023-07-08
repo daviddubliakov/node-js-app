6f248e4c4c5d48998047346a45cdbcfb404290f6
david.dub <david.dub@brainstack.team>
2023-07-09 00:28:11 +0300

    adds generating pdf files

--- controllers/shop.js
+++ controllers/shop.js
@@ -167,13 +167,24 @@ exports.getInvoice = (req, res, next) => {
       const pdfDoc = new PDFDocument();
 
       res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader(
-        'Content-Disposition',
-        `attachment; filename=${invoiceName}`,
-      );
+      res.setHeader('Content-Disposition', `inline; filename=${invoiceName}`);
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
-      pdfDoc.text('Hello World!');
+      pdfDoc.fontSize(26).text('Invoice', { underline: true });
+      pdfDoc.text('-------------------------');
+
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            `${prod.product.title} - ${prod.quantity} x $${prod.product.price}`,
+          );
+      });
+      pdfDoc.text('----');
+      pdfDoc.fontSize(20).text(`Total price: $${totalPrice}`);
+
       pdfDoc.end();
 
       // fs.readFile(invoicePath, (err, data) => {
