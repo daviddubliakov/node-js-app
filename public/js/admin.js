fbeb9b03096fa93490131106bb9f2e23a246c118
david.dub <david.dub@brainstack.team>
2023-08-09 19:05:53 +0300

    adds dom manipulating

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,13 +1,16 @@
 const deleteProduct = (btn) => {
   const productId = btn.parentNode.querySelector('[name=productId]').value;
   const csrfToken = btn.parentNode.querySelector('[name=_csrf]').value;
+  const proudctElement = btn.closest('article');
 
   fetch(`/admin/product/${productId}`, {
     method: 'DELETE',
     headers: { 'csrf-token': csrfToken },
   })
-    .then((res) => {
-      console.log(res);
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      proudctElement.parentNode.removeChild(proudctElement);
     })
     .catch((err) => console.log(err));
 };
